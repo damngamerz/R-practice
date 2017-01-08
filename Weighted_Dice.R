c4f6e311f0eed2bc3940dfe8a3e448a491b881a8
damngamerz <sauravsinghshakya@yahoo.com>
2017-01-08 23:43:37 +0530

    8 Jan

--- Weighted_Dice.R
+++ Weighted_Dice.R
@@ -6,3 +6,14 @@ roll<-function(){
 }
 rolls<-replicate(1000,roll())
 qplot(rolls,binwidth=1)
+
+
+#Expected Value of Weighted Die.
+die<-1:6
+rolls<-expand.grid(die,die)
+rolls$value<-rolls$Var1+rolls$Var2
+prob<-c("1"=1/8,"2"=1/8,"3"=1/8,"4"=1/8,"5"=1/8,"6"=3/8)
+rolls$prob1<-prob[rolls$Var1]
+rolls$prob2<-prob[rolls$Var2]
+rolls$prob<-rolls$prob1*rolls$prob2
+sum(rolls$value*rolls$prob)
